0bd23bcfb64d9ef233c80291b11796a8b0f7fea7
colinale <colinale@buffalo.edu>
2022-08-24 20:37:47 -0400

    updates to htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 
-// "/notes" responds with the notes.html file
+// "/notes" responds with the notes.html file // 
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
